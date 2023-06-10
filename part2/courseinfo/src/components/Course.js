9abe3d26db9d9a7acab08145203238be7f7352a7
Jason Zheng <JasonZheng892@gmail.com>
2023-06-10 04:20:29 -0400

    Added courseinfo

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,51 @@
+const Header = ({course}) => {
+    return (
+      <>
+        <h2>
+          {course}
+        </h2>
+      </>
+    )
+  }
+
+const Part = ({part}) => {
+  return (
+    <>
+      <p>
+        {part.name} {part.exercises} 
+      </p>
+    </>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <>
+      {parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </>
+  )
+}
+
+const Total = ({parts}) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <>
+      <p><b>total of {totalExercises} exercises</b></p>
+    </>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
